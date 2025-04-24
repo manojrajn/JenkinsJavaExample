e729e244ca7701dd1daec8906109bbc286e1fc1a
manojrajn <43582455+manojrajn@users.noreply.github.com>
2025-04-24 17:13:27 +0530

    Jenkins autorun check

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
   public static void main(String args[]) {
     for(int i = 0; i < 5; i++)
       System.out.println(i);
+
+    System.out.println("Doing a new commit to check Jenkins build triggered");
   }
 }
